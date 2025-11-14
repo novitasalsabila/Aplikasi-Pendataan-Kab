1ebcd06fb7c31dba915c4627145787b7cc6e138f
Adisti Ode <ode.adisti1999@gmail.com>
2025-11-14 11:44:33 +0700

    benerin nama  relasi ke department_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
 
     public function departement()
     {
-        return $this->belongsTo(Department::class, 'departement_id');
+        return $this->belongsTo(Department::class, 'department_id');
     }
 
 }
