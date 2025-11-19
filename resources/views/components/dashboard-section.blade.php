543ff2b6c27cb64e050dc93dfc80c355391ec149
Asfanissa Ainun Salsabila <121386693+Asfanissa07@users.noreply.github.com>
2025-11-19 08:57:36 +0700

    menghapus mode dark di component dashboard section

--- resources/views/components/dashboard-section.blade.php
+++ resources/views/components/dashboard-section.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white dark:bg-gray-800 shadow-md rounded-lg mt-8">
+<div class="bg-white shadow-md rounded-lg mt-8">
     <div class="p-6">
-        <h2 class="text-lg font-bold mb-4 text-gray-800 dark:text-gray-100">{{ $title }}</h2>
+        <h2 class="text-lg font-bold mb-4 text-gray-800">{{ $title }}</h2>
         {{ $slot }}
     </div>
 </div>
