b31c4174712d20438bdd5aa577e5cb08c798c596
Adisti Ode <ode.adisti1999@gmail.com>
2025-11-24 10:48:05 +0700

    mengubah tombol lihat edit hapus (developers)

--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -30,7 +30,7 @@
 <div class="p-4 bg-white shadow-sm rounded-lg border border-gray-200 mb-6">
         <div class="flex flex-col sm:flex-row sm:items-center mb-6 gap-3 w-full">
             <!-- Form Search & Filter -->
-            <form action="{{ route('applications.index') }}" method="GET" class="flex flex-col sm:flex-row flex-wrap gap-2 w-full">
+            <form action="{{ route('developers.index') }}" method="GET" class="flex flex-col sm:flex-row flex-wrap gap-2 w-full">
 
                 <!-- Input + Tombol Search -->
                 <div class="relative flex-1">
@@ -75,25 +75,67 @@
                             <td class="px-4 py-3 font-medium">{{ $dev->name }}</td>
                             <td class="px-4 py-3 capitalize">{{ $dev->developer_type }}</td>
                             <td class="px-4 py-3">{{ $dev->contact_email ?? '-' }}</td>
-                            <td class="px-4 py-3">{{ $dev->contact_phone ?? '-' }}</td>
-                            <td class="px-4 py-3 text-center space-x-2">
-                                    <a href="{{ route('developers.show', $dev->id) }}"
-                    class="text-blue-600 hover:text-blue-700 font-semibold">Lihat</a>
-                                <a href="{{ route('developers.edit', $dev->id) }}"
-                                   class="text-yellow-600 hover:text-yellow-700 font-semibold">
-                                   Edit
-                                </a>
-                                <form action="{{ route('developers.destroy', $dev->id) }}" method="POST"
-                                      class="inline"
-                                      onsubmit="return confirm('Yakin ingin menghapus {{ $dev->name }}?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                            class="text-red-600 hover:text-red-700 font-semibold">
-                                        Hapus
-                                    </button>
-                                </form>
-                            </td>
+                            <td class="px-4 py-3">{{ $dev->contact_phone ?? '-' }}</td>             
+                            <td class="px-4 py-3 text-center space-x-3 flex items-center justify-center">
+                                <!-- Lihat -->
+                                <a href="{{ route('developers.show', $dev->id) }}" 
+                                class="text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center px-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                                stroke-width="1.5" stroke="currentColor" class="size-5">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5
+                                c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639
+                                C20.577 16.49 16.64 19.5 12 19.5
+                                c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" 
+                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        </a>
+                        <!-- Edit -->
+                        <a href="{{ route('developers.edit', $dev->id) }}"
+                        class="text-yellow-600 hover:text-yellow-700 font-semibold inline-flex items-center px-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                        stroke-width="1.5" stroke="currentColor" class="size-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" 
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652
+                        L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18
+                        l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Z
+                        M19.5 7.125 16.862 4.487M18 14v4.75
+                        A2.25 2.25 0 0 1 15.75 21H5.25
+                        A2.25 2.25 0 0 1 3 18.75V8.25
+                        A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+                </a>
+                <!-- Hapus -->
+                <form id="deleteForm-{{ $dev->id }}" 
+                    action="{{ route('developers.destroy', $dev->id) }}" 
+                    method="POST" class="inline px-3">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button"
+                        onclick="openModal('{{ $dev->id }}')" 
+                        class="text-red-600 hover:text-red-700 font-semibold inline-flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                                stroke-width="1.5" stroke="currentColor" class="size-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" 
+                                d="m14.74 9-.346 9m-4.788 0L9.26 9
+                                m9.968-3.21c.342.052.682.107 1.022.166
+                                m-1.022-.165L18.16 19.673
+                                a2.25 2.25 0 0 1-2.244 2.077H8.084
+                                a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79
+                                m14.456 0a48.108 48.108 0 0 0-3.478-.397
+                                m-12 .562c.34-.059.68-.114 1.022-.165
+                                m0 0a48.11 48.11 0 0 1 3.478-.397
+                                m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201
+                                a51.964 51.964 0 0 0-3.32 0
+                                c-1.18.037-2.09 1.022-2.09 2.201v.916
+                                m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                    </button>
+                </form>
+
+</td>
+
                         </tr>
                     @empty
                         <tr>
