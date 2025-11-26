549a8468f6fafcafba783f46eb4607cc51f5fd4a
novitasalsabila <novnovv10@gmail.com>
2025-11-26 13:30:26 +0700

    Update nontifikasi

--- resources/views/application_documents/index.blade.php
+++ resources/views/application_documents/index.blade.php
@@ -19,9 +19,12 @@
                 </p>
             </div>
         </div>
-
         @if (session('success'))
-            <div class="mb-4 bg-green-100 border border-green-400 text-green-800 px-4 py-3 rounded">
+            <div x-data="{ show: true }"
+                x-show="show"
+                x-transition
+                x-init="setTimeout(() => show = false, 2500)"
+                class="bg-green-100 border border-green-400 text-green-800 px-4 py-3 rounded mb-4">
                 {{ session('success') }}
             </div>
         @endif
